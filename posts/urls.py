5d5caadcff2dcdf8a8af52a9c33a85b0e93a4b4d
shabeeb <shabeebtk07@gmail.com>
2026-04-17 20:40:42 +0530

    added post delete API

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from posts.views.posts_views import CreatePostAPIView, ListPostsAPIView
+from posts.views.posts_views import CreatePostAPIView, ListPostsAPIView, DeletePost
 from posts.views.like_views import ToggleLikeAPIView, ListPostLikesAPIView
 from posts.views.comments_views import ListCommentsAPIView, CreateCommentAPIView, ListRepliesAPIView
 
@@ -8,8 +8,11 @@ from posts.views.comments_views import ListCommentsAPIView, CreateCommentAPIView
 urlpatterns = [
     path('create', CreatePostAPIView.as_view(), name='create-post'),
     path('list', ListPostsAPIView.as_view(), name='list-posts'),
+    path('delete', DeletePost.as_view(), name='delete-posts'),
+
     path('like', ToggleLikeAPIView.as_view(), name='toggle-like'),
     path('likes/list', ListPostLikesAPIView.as_view(), name='list-likes'),
+
     path('comments/create', CreateCommentAPIView.as_view(), name='create-comment'),
     path('comments/list', ListCommentsAPIView.as_view(), name='list-comments'), # add replies data here itself
     path('comments/list/replies', ListRepliesAPIView.as_view(), name='list-comment-replies'),
